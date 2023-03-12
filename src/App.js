e1d4aecbc6c2747bf65b9e3ce70663eab8796142
HDJ <johorea1356@gmail.com>
2023-03-12 22:02:34 +0900

    기본 세팅

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+   return (
+      <div class="app-style">
+         <h1>헤딩 태그</h1>
+      </div>
+   );
+}
+
+export default App;
