2e131236de0cd8776d4ac32b714989ae36f92d73
HDJ <johorea1356@gmail.com>
2023-03-15 16:00:56 +0900

    map을 이용한 배열 값 변경

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import "./App.css";
 
 function App() {
-   let list = [1, 2, 3];
+   const list = [
+      { id: 1, name: "aaa" },
+      { id: 2, name: "bbb" },
+      { id: 3, name: "ccc" },
+   ];
+   const newName = { name: "kkk" };
+   const listNo1 = { ...list[1], ...newName }; //배열 특정 인덱스만 값 바꾸기
+   console.log(list);
+   console.log(listNo1);
+   const updateNameDto = {
+      id: 3,
+      name: "ppp",
+   };
+   //배열에서 특정 key의 value 변경하기
+   const newList = list.map(index => (index.id === updateNameDto.id ? { ...index, ...updateNameDto } : index));
+   const jsonStr = JSON.stringify(newList);
    return (
       <div class="app-style">
-         <h1>
-            {list.map(i => (
-               <div>{i}</div>
-            ))}
-         </h1>
+         <h1>{jsonStr}</h1>
       </div>
    );
 }
